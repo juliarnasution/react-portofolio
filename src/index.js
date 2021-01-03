f694305b912382278544d11460ae43da7a06c7dc
junas <juliarnasution@gmail.com>
2021-01-03 07:59:44 +0700

    fix error React Refresh runtime should not be included in the production bundle.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+if (process.env.NODE_ENV === 'development') {
+  if (module.hot) {
+    module.hot.accept();
+  }
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
